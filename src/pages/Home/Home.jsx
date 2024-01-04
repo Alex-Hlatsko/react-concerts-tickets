a85b8d48b46982a05c375223db063e676454c370
Alen-Berg <alexei.hlatsko@gmail.com>
2024-01-04 09:00:36 +0100

    fix admin and btn

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Navigation from '../../components/Navigation/Navigation'
 import './style.css'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <>
-      <Navigation/>
-      <div className="heading">
-        <h1 className=''>Rhythmic Revolution</h1>
-        <p>More Than Music: A Concert Adventure Tailored to Enchant, Captivate, and Inspire</p>
-        <a href="/concerts">Let's go</a>
+      <Navigation />
+      <div className="flex flex-col items-center h-screen">
+        <div className="heading">
+          <h1 className=''>Rhythmic Revolution</h1>
+          <p>More Than Music: A Concert Adventure Tailored to Enchant, Captivate, and Inspire</p>
+          <Link to="/concerts">Let's go</Link>
+        </div>
+        <Link className='text-gray-600 mt-8' to="/admin">Admin Panel</Link>
       </div>
     </>
   )
